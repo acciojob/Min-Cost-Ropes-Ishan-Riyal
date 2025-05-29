6587c944c697389bf9931342cf80cbba5b911059
Ishan Riyal <ishanriyal07@gmail.com>
2025-05-30 01:07:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-function mincost(arr)
-{ 
+function mincost(arr){ 
 //write your code here
+	let totalCost = 0;
+
+	while(arr.length > 1){
+		arr.sort((a,b) => a-b);
+
+		let first = arr.shift();
+		let second = arr.shift();
+
+		let cost = first + second;
+		totalCost += cost;
+
+		arr.push(cost)
+	}
 // return the min cost
+	return totalCost;
   
 }
 
